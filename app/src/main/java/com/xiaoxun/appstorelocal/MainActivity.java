2d16f11f2962594428eb65f1f260618ef2d16a4e
niukangwei <2314504058@qq.com>
2023-12-26 16:21:20 +0800

    创建应用商店仓库

---
+++ app/src/main/java/com/xiaoxun/appstorelocal/MainActivity.java
@@ -0,0 +1,21 @@
+package com.xiaoxun.appstorelocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xiaoxun.appstorelocal.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
